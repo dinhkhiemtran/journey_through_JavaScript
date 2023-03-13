b723f0b63b3dbd957d2bdf95c0e7beceb0dc3e21
Khiem Tran <tdkhiem>
2023-03-13 11:41:28 +0700

    excersim/05

---
+++ src/main/array-analysis/enchantments.js
@@ -0,0 +1,103 @@
+// @ts-check
+//
+// The line above enables type checking for this file. Various IDEs interpret
+// the @ts-check directive. It will give you helpful autocompletion when
+// implementing this exercise.
+
+/**
+ * Get the position (index) of the card in the given stack
+ *
+ * @param {number[]} stack
+ * @param {number} card
+ *
+ * @returns {number} position of the card in the stack
+ */
+export function getCardPosition(stack, card) {
+  let index = -1;
+  for (let i = 0; i < stack.length; i++) {
+    if (stack[i] === card) {
+      index = i;
+    }
+  }
+  return index;
+}
+
+function getCardPositionIndexOf(stack, card) {
+  return stack.indexOf(card);
+}
+
+const card = 2;
+let numbNeeded = getCardPosition([9, 7, 3, 2], card);
+console.log(numbNeeded);
+
+/**
+ * Determine if the stack contains the card
+ *
+ * @param {number[]} stack
+ * @param {number} card
+ *
+ * @returns {boolean} true if card is in the stack, false otherwise
+ */
+export function doesStackIncludeCard(stack, card) {
+  for (let i = 0; i < stack.length; i++) {
+    if (stack.includes(card)) {
+      return true;
+    }
+  }
+  return false;
+}
+const card1 = 3;
+const isPresent = doesStackIncludeCard([2, 3, 4, 5], card1);
+console.log(isPresent);
+
+/**
+ * Determine if each card is even
+ *
+ * @param {number[]} stack
+ *
+ * @returns {boolean} true if all cards are even, false otherwise
+ */
+export function isEachCardEven(stack) {
+  return stack.every((e) => e % 2 === 0);
+}
+const isEven = isEachCardEven([2, 4, 6, 7]);
+console.log(isEven);
+/**
+ * Check if stack contains odd-value card
+ *
+ * @param {number[]} stack
+ *
+ * @returns {boolean} true if the array contains odd card, false otherwise
+ */
+export function doesStackIncludeOddCard(stack) {
+  return stack.some((e) => e % 2 !== 0);
+}
+const isOdd = doesStackIncludeOddCard([3, 2, 6, 4, 8]);
+console.log(isOdd);
+
+/**
+ * Get the first odd card from the stack
+ *
+ * @param {number[]} stack
+ *
+ * @returns {number | undefined} the first odd value
+ */
+export function getFirstOddCard(stack) {
+  return stack.find(e => e % 2 !== 0);
+}
+
+const numberNeeded1 = getFirstOddCard([4, 2, 8, 7, 9]);
+console.log(numberNeeded1);
+
+/**
+ * Determine the position of the first card that is even
+ *
+ * @param {number[]} stack
+ *
+ * @returns {number} position of the first card that is even
+ */
+export function getFirstEvenCardPosition(stack) {
+   return stack.findIndex(e => e % 2 === 0);
+}
+const numberNeeded2 = getFirstEvenCardPosition([5, 2, 3, 1]);
+console.log(numberNeeded2);
---
+++ src/main/array-loops/enchantments.js
@@ -0,0 +1,41 @@
+// @ts-check
+
+/**
+ * Determine how many cards of a certain type there are in the deck
+ *
+ * @param {number[]} stack
+ * @param {number} card
+ *
+ * @returns {number} number of cards of a single type there are in the deck
+ */
+export function cardTypeCheck(stack, card) {
+    let count = 0;
+    stack.forEach((e) => e === card ? count++ : 0);
+    return count;
+}
+
+const cardType = 3;
+let countCards = cardTypeCheck([1, 2, 3, 4], cardType);
+console.log(countCards);
+
+/**
+ * Determine how many cards are odd or even
+ *
+ * @param {number[]} stack
+ * @param {boolean} type the type of value to check for - odd or even
+ * @returns {number} number of cards that are either odd or even (depending on `type`)
+ */
+export function determineOddEvenCards(stack, type) {
+    let rest = type ? 0 : 1;
+    let count = 0;
+    for (let e of stack) {
+      e % 2 === rest ? count++ : 0; 
+    }
+    return count;
+}
+const test1 = determineOddEvenCards([1, 2, 3, 1, 5, 6], true);
+// => 2
+console.log(test1);
+const test2 = determineOddEvenCards([1, 2, 3, 1, 5, 6], false);
+// => 4
+console.log(test2);
